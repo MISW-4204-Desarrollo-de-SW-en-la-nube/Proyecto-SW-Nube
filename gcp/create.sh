d838f99db0f45ea1c732383084853f11a404e2f6
Nicolas Potier Anzola <n.potier@uniandes.edu.co>
2024-04-20 13:26:14 -0500

    test/solo nfs server

--- gcp/create.sh
+++ gcp/create.sh
@@ -56,12 +56,13 @@ gcloud compute instances create $NFS_INSTANCE_NAME \
     --metadata=startup-script='#! /bin/bash
     sudo apt update && sudo apt install -y nfs-kernel-server
     sudo mkdir -p /nube/public
-    sudo chown nobody:nogroup /nube/public
-    sudo chmod 777 /nube/public
-    echo "/nube/public *(rw,sync,no_subtree_check)" | sudo tee -a /etc/exports
-    sudo exportfs -a
-    sudo systemctl restart nfs-kernel-server
     '
+    # sudo mkdir -p /nube/public
+    # sudo chown nobody:nogroup /nube/public
+    # sudo chmod 777 /nube/public
+    # echo "/nube/public *(rw,sync,no_subtree_check)" | sudo tee -a /etc/exports
+    # sudo exportfs -a
+    # sudo systemctl restart nfs-kernel-server
 
 # AÑADIR TAGS A LA INSTANCIA NFS
 gcloud compute instances add-tags $NFS_INSTANCE_NAME \
