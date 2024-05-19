90cfcda0ce0596c614bdd0ca337cf98b21615696
Nicolas Potier Anzola <n.potier@uniandes.edu.co>
2024-05-19 03:02:42 -0500

    update sources 8

--- gcp/create.sh
+++ gcp/create.sh
@@ -204,12 +204,26 @@ gcloud sql instances create $DB_INSTANCE_NAME \
     --no-assign-ip \
     --network default
 
+# gcloud sql instances create mv2-db \
+#     --database-version=POSTGRES_15 \
+#     --root-password=12345678 \
+#     --region=us-west1 \
+#     --storage-size=10GB \
+#     --no-storage-auto-increase \
+#     --memory=3.75GB \
+#     --cpu=1 \
+#     --no-assign-ip \
+#     --network=default
+
 # ESPERAR A QUE SE CREE LA INSTANCIA
 sleep 60
 
 # Obtener la IP privada de la instancia de Cloud SQL
-DB_PRIVATE_IP=$(gcloud sql instances describe $DB_INSTANCE_NAME --format="value(ipAddresses.ipAddress)" --filter="type:PRIVATE")
+DB_PRIVATE_IP=$(gcloud sql instances describe $DB_INSTANCE_NAME --format="value(ipAddresses.ipAddress)")
 echo "Private IP of Cloud SQL instance: $DB_PRIVATE_IP"
+
+# # OBTENER LA IP DE LA BASE DE DATOS
+# DB_IP=$(gcloud sql instances describe mv2-db --format='value(ipAddresses.ipAddress)')
 
 # ## ==================== BASE DE DATOS ====================
 
